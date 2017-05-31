2a98715f68119ba86e0d7edd2dc5330aa2dde8a7
Ines Pucker <ines.pucker@gmx.de>
2017-05-31 16:26:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // code solution here
+class President {
+  constructor(name, politicalParty, yearsInOffice, homeState) {
+    this.name = name;
+    this.politicalParty = politicalParty;
+	this.yearsInOffice = yearsInOffice;
+	this.homeState = homeState;
+  }
+  veto() {
+	return "NO!"
+  }
+
+  passBill() {
+	return "You can do that!"
+  }
+
+ doCharity() {
+	return "I like to help people."
+  }
+
+  conductPressInterview() {
+	return "I am proud to be an American."
+  }
+
+   sayHi() {
+	return ("Hi, my name is " + this.name + ". I am from " + this.homeState + ". I represent the " + this.politicalParty + "s, and was in office " + this.yearsInOffice + "." )
+  }
+}
+
+
+
+washingtonGeorge = new President("George Washington", "Democrats and Republic", "1789-1797", "Virginia")
+
+lincolnAbraham =  new President("Abraham Lincoln", "Republic", "1861-1865", "Kentucky")
+
+
+nixonRichard =  new President("Richard Nixon", "Republic", "1969-1974", "California")
+
+
+carterJimmy = new President("Jimmy Carter", "Democrat", "1977-1981", "Georgia")
